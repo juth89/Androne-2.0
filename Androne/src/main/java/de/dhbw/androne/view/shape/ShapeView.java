d3a0afe02b5ee0437fd73b13da4ad23c92f41662
juth89 <Julian.Thiel@gmx.de>
2014-04-17 10:18:17 +0200

    Organized imports

--- Androne/src/main/java/de/dhbw/androne/view/shape/ShapeView.java
+++ Androne/src/main/java/de/dhbw/androne/view/shape/ShapeView.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Paint.Align;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
